da58e862f6fdcef750caab5539e1bc18541539f1
MiguelTamayo <34068757+MiguelTamayo@users.noreply.github.com>
2020-10-14 10:35:24 -0500

    Added Team Splash Screen, Partial Start Menu
    added TextMeshPro for fonts and ui. Added GameSceneManager & AudioManager classes

---
+++ GameProject/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    // Singleton
+    public static GameSceneManager Instance = null;
+    
+    private void Awake()
+    {
+        Instance = this;
+
+        if (SceneManager.GetActiveScene().name == "TeamSplashScreen") 
+        {
+            Invoke("loadStartMenu", 4f);
+        }
+    }
+
+    void loadStartMenu()
+    {
+        LoadNextScene("StartMenu");
+    }
+
+    public void LoadNextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
