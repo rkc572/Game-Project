6cf847cc88c19cf79eef2d2b6b6e83139aa7b5a9
Joshua <Joshua@Joshua-Dickens.com>
2020-10-31 17:41:04 -0800

    Added Volume Mixer and Associated Sliders

---
+++ GameProject/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public string audioName = "MasterVol";
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat(audioName, Mathf.Log10(sliderValue) * 20);
+    }
+}
