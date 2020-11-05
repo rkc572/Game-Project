fb32e0e960a2c34c60b4908f0fc59e0ccaed6cb3
A-Baji <bidabaji@gmail.com>
2020-11-04 19:46:54 -0600

    set up scenes for forest

---
+++ GameProject/Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public GameSceneManager sceneManager;
+    public string scene;
+
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            sceneManager.LoadNextScene(scene);
+        }
+    }
+
+    void PickRandomScene(string[] sceneList)
+    {
+        System.Random rnd = new System.Random();
+        int num = rnd.Next(0, sceneList.Length);
+        scene = sceneList[num];
+    }
+
+    private void Awake()
+    {
+        if (scene == "Random")
+        {
+            PickRandomScene(new string[] { "Forest scene 3", "Forest scene 4", "Forest scene 5" });
+        }
+    }
+}
