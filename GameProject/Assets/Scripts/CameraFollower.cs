4aec0d6862be8e3d0ee9e91e90e6efc0339126b6
MiguelTamayo <34068757+MiguelTamayo@users.noreply.github.com>
2020-11-06 23:43:18 -0600

    added forest camp scene

---
+++ GameProject/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
