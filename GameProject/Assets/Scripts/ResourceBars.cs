b98dfc56207883cb3390e84702bc24af8b3e346d
A-Baji <bidabaji@gmail.com>
2020-11-07 23:47:38 -0600

    finished health bar, mana bar, and gold counter

---
+++ GameProject/Assets/Scripts/ResourceBars.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceBars : MonoBehaviour
+{
+    public Slider bar;
+    [Tooltip("True for health, false for mana")]
+    public bool resourceType;
+    Player player;
+    float resource;
+
+    void Start ()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    void Update ()
+    {
+        if (resourceType)
+            resource = player.GetComponent<Mob>().health;
+        else
+            resource = player.GetComponent<Mob>().mana;
+        bar.value = resource;
+    }
+}
