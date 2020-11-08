4b067c2276d3e96936ab9efd0b7b31debd747aa7
Joshua <Joshua@Joshua-Dickens.com>
2020-11-08 13:33:42 -0800

    Added in health/mana potions
    Added in new scripts for both health and mana drops, adjusted playersounds to accommodate their sound effects, created prefabs for potion drops.

---
+++ GameProject/Assets/Scripts/ManaDrop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaDrop : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public float timeToDestroy = 10.0f;
+    float timeSpawned;
+    bool flick = false;
+    public float value = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSpawned = Time.time;
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            var player = collision.GetComponent<Player>();
+            player.properties.propertiesManager.ModifyManaByAmount(value);
+            player.sounds.PlayManaPickupSFX();
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // start blinking once only 25% time left
+        if (Time.time - timeSpawned > timeToDestroy * 0.75f)
+        {
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, flick ? 1.0f : 0.5f);
+            flick = !flick;
+        }
+    }
+}
