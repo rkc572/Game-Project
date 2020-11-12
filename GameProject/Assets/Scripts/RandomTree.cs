d8ae03aff619a94e7c47a401b477b886e214ea29
A-Baji <bidabaji@gmail.com>
2020-11-12 13:55:55 -0600

    created randomtree prefab with the full tree sprites

---
+++ GameProject/Assets/Scripts/RandomTree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTree : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteList;
+
+    void ChooseSprite()
+    {
+        System.Random rnd = new System.Random();
+        int num = rnd.Next(0, 18);
+        spriteRenderer.sprite = spriteList[num];
+    }
+
+void Start()
+    {
+        ChooseSprite();
+    }
+}
