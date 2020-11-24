276b983c37805dbb911e9345e05d55526f4bd21d
A-Baji <bidabaji@gmail.com>
2020-11-23 21:58:13 -0600

    potion hud finished
    - need to create sprites for other potions. using sword sprite as placeholder
    
    - need to finish Potion.cs
    
    - need function somewhere to add potions to player potion list.

--- GameProject/Assets/Scripts/Potion.cs
+++ GameProject/Assets/Scripts/Potion.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Potion : MonoBehaviour
 {
+    public string type; // health, mana, strength, magboost, speed, regen
+    public int count;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,33 @@ public class Potion : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public Potion (string t, int c)
+    {
+        this.type = t;
+        this.count = c;
+    }
+
+    public void UsePotion()
+    {
+        // grant effect depending on potion type
+        //switch (this.type)
+        //{
+        //    case "health":
+        //        break;
+        //    case "mana":
+        //        break;
+        //    case "strength":
+        //        break;
+        //    case "magboost":
+        //        break;
+        //    case "speed":
+        //        break;
+        //    case "regen":
+        //        break;
+        //}
+        this.count--;
+        Debug.Log("use potion");
     }
 }
