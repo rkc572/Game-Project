c233cf492ad0e6a22014d17f532bc21b615e7e1e
A-Baji <bidabaji@gmail.com>
2020-11-25 19:52:15 -0600

    sub dungeons layout complete

---
+++ GameProject/Assets/BlockedWalls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockedWalls : MonoBehaviour
+{
+    public bool entranceLeft;
+    public bool entranceRight;
+    public bool trigger;
+    public bool hasItem;
+
+    
+    void Update()
+    {
+        if (entranceLeft)
+        {
+            if (Player.Instance.transform.position.x > transform.position.x + 0.075f && !hasItem) // Close passage when entering from left
+            {
+                transform.GetChild(0).gameObject.SetActive(true);
+                transform.GetChild(1).gameObject.SetActive(true);
+            }
+        }
+        else if (entranceRight)
+        {
+            if (Player.Instance.transform.position.x < transform.position.x - 0.075f && !hasItem) // Close passage when entering from right
+            {
+                transform.GetChild(0).gameObject.SetActive(true);
+                transform.GetChild(1).gameObject.SetActive(true);
+            }
+        }
+        if (trigger || hasItem) // Open passage when room is complete (trigger) or once the item for the sub dungeon has been acquired (hasItem)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+    }
+}
