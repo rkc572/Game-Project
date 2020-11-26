34bcd92c94028ad7e14f42b767f95c04e3f6be0e
A-Baji <bidabaji@gmail.com>
2020-11-25 21:42:10 -0600

    only missing mobs and upgrade ui at the end
    Need to rewrite 2 and 3 (and possibly 1) to be reusabled for other sub dungeons

---
+++ GameProject/Assets/Scripts/BootsRoomsTriggers/Boots3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boots3 : MonoBehaviour
+{
+    public BlockedWalls left;
+    int index;
+
+    void Start()
+    {
+        index = Player.Instance.artifacts.IndexOf(Player.Instance.playerDashBoots);
+    }
+
+    void Update()
+    {
+        if (index >= 0)
+        {
+            if (Player.Instance.artifacts[index].elementalAttribute != ElementalAttribute.NONE)
+            {
+                left.hasItem = true;
+            }
+        }
+    }
+}
